4eae54d0e50ec90d73302640e60fdb317b96b787
Adrian <adrian@gemini-gaming.com>
2026-01-27 11:01:07 +0800

    feat: added cors
    - to make communications between frontend and backend
    - add notes for future use
    - update gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@
 
 # FastAPI imports
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # For template rendering (if needed). To load HTML templates
 from fastapi.templating import Jinja2Templates
@@ -23,6 +24,21 @@ app.include_router(user.router, prefix="", tags=["User Management"])
 
 # Templates directory setup
 templates = Jinja2Templates(directory="app/templates")
+
+# Define the origins that are allowed to talk to your API
+origins = [
+    "http://localhost:5173",  # Vite default
+    "http://127.0.0.1:5173",
+]
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],    # Allows GET, POST, OPTIONS, etc.
+    allow_headers=["*"],    # Allows custom headers like Authorization
+)
 
 @app.get("/test-main")
 async def test_main():
