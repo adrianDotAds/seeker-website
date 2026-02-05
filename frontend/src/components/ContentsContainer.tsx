0a92fa949dfaf976a77aa7c0763528579dcebdfd
Adrian <adrian@gemini-gaming.com>
2026-02-05 09:34:05 +0800

    style: container display
    - hides and unhides dive based on button click

--- frontend/src/components/ContentsContainer.tsx
+++ frontend/src/components/ContentsContainer.tsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
 
 function ContentsContainer({ activeButton }) {
+    const [hide, setHide] = useState(false);
+
+    const toggleHide = () => {
+        if (activeButton === 'LOGO') {
+            setHide(!hide);
+        } else {
+            setHide(false);
+        }
+    };
+
+    const mainSelected = {
+        display: 'flex',
+        flexDirection: 'column' as 'column',
+        alignItems: 'center' as 'center',
+        justifyContent: 'center' as 'center',
+    };
+
+    const contentsSelected = {
+        display: 'flex',
+        flexDirection: 'column' as 'column',
+        alignItems: 'center' as 'center',
+        justifyContent: 'center' as 'center',
+    };
+
     return (
-        <div className="contents-container">
-            {activeButton === 'QUEST' && <div>Quest Content</div>}
-            {activeButton === 'SUBGUILDS' && <div>Subguilds Content</div>}
-            {activeButton === 'RANKERS_HALL' && <div>Rankers Hall Content</div>}
-            {activeButton === 'SCROLLS' && <div>Scrolls Content</div>}
-            {activeButton === 'CODEX' && <div>Codex Content</div>}
-        </div>
+        <>
+            <div className="contents-container" style={{ display: activeButton === 'LOGO' ? 'none' : 'block' }}>
+                {activeButton === 'QUEST' && <div>Quest Content</div>}
+                {activeButton === 'SUBGUILDS' && <div>Subguilds Content</div>}
+                {activeButton === 'RANKERS_HALL' && <div>Rankers Hall Content</div>}
+                {activeButton === 'SCROLLS' && <div>Scrolls Content</div>}
+                {activeButton === 'CODEX' && <div>Codex Content</div>}
+            </div>
+            <div className="home-container" style={{display: activeButton === 'LOGO' ? 'block' : 'none'}}>
+                {activeButton === 'LOGO' && <div>Our Journey</div>}
+            </div>
+        </>
+            
     );
 }
 
